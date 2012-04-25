fd0186ef8be1717c1664cfcbb880ac83b52c0e2c
asd5640252 <asd5640252@163.com>
2012-04-26 00:12:30 +0800

    ....

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
+
+/**
+ * 判断执行模块
+ * @index {[number]} 0代表主域名，1代表二级域名，。。。以此类推。
+ */
+function getHostName(index) {
+    var domain = document.domain,
+        ArrHost = domain.split("."),
+        index = index ? parseInt(index) : 0,
+        item = ArrHost.length - 2 - index;
+        if (item < 0) {
+            return "";
+        }
+        else {
+            return ArrHost[item].toString();
+        }
+}
+
 /**
  * 新浪爱问
  */
+var Isak = (function(){
+    var douban_url = window.location.href,
+        reg = /(\d{7,8})/g;  
+    this.douban_id = douban_url.match(reg);
+    this.title = $('html head title').text();
+    this.keyword1 = this.title.replace( '(豆瓣)', '' ).trim();  
+    this.keyword2 = encodeURIComponent( this.keyword1 );
+    this.unitname = new Array('Y','Z','E','P','T','G','M','K');
+    this.unitsize = new Array(1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024 * 1024,1024 * 1024 * 1024,1024 * 1024,1024);
+    return this;
+})();
 
 var dbIsak = function() {
         var title = $('html head title').text(),
@@ -50,6 +79,57 @@ var dbIsak = function() {
                 }
             }
         });
+}
+
+var dbIsakMovie = function(){
+    var url = 'http://sikemi.sinaapp.com/douban_api.php?keyword=' + Isak.keyword2;
+    var html_title = '<div class="da3" style="margin-bottom:0px;padding-bottom:1px;"><h2>可下载的资源&nbsp; ·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;<span class="pl">(<a href="http://torrentproject.com/?s=' + Isak.keyword2 + '&btnG=Torrent+Search&num=20&start=0" target="_blank">全部</a>)</h2></span></div>'
+    var html_body_start = '<div class="indent" id="db-doulist-section" style="padding-left:5px;border:1px #F4F4EC solid;"><ul class="bs">';
+    var html_body_yes = '';
+    var html_body_no = '<li>没有找到相关资源，手动去<a href="http://torrentproject.com/?s='+Isak.keyword2+'&btnG=Torrent+Search&num=20&start=0" target="_blank">搜索</a></li>';
+    var html_body_end = '</ul>';
+    var html_body_endmore = '<div style="text-align:right; padding:5px 10px 5px 0px;"><a href="http://ratwu.com/2012/03/douban_download_plugin/" target="_blank">反馈</a></div>';
+    var html_body_endend = '</div>';
+    var length = 30;
+    $.getScript(url, function(data, textStatus, jqxhr) {
+       console.log(data); //data returned
+       console.log(textStatus); //success
+       console.log(jqxhr.status); //200
+       console.log('Load was performed.');
+    });
+    $.ajax(
+        {
+            type : 'GET',
+            dataType : 'script',
+            url : url,
+            success : function(data) {
+                alert(data);
+                if( iaskSearchResult.sp.m > 0 ) {
+                    var title,title2, image, filesize, url, unit;
+                    var regex = /([A-Z\u0391-\uffe5])/g;
+                    for( key in iaskSearchResult.sp.result ) {
+                        title = iaskSearchResult.sp.result[key].title;
+                        title2 = Isak.title.replace( regex, "$1*" );
+                        ellipsis = title2.length > length ? '..' : '' ;
+                        title2 = title2.substr( 0, length ).replace( /\*/g, '' ) + ellipsis;
+                        filesize = iaskSearchResult.sp.result[key].filesize;
+                        seeds    = iaskSearchResult.sp.result[key].seeds;
+                        if( filesize < 1024 ) filesize = filesize+'B';
+                        for( var i=0; i<Isak.unitname.length; i++ ){
+                            if( filesize > Isak.unitsize[i] || filesize==Isak.unitsize[i] ){
+                                filesize = Math.round( filesize / Isak.unitsize[i] * 10 ) / 10 + Isak.unitname[i];
+                            }
+                        }
+                        url = iaskSearchResult.sp.result[key].url;
+                        html_body_yes += '<li><a href="' + url + '?from=douban" title="' + title + '" target="_blank">' + title2 + '</a><span class="pl">(' + filesize + '&nbsp;种子:' + seeds + ')</span></li>';
+                    }
+                    $( '.aside' ).prepend( html_title + html_body_start + html_body_yes + html_body_end + html_body_endmore + html_body_endend );
+                } else {
+                    $( '.aside' ).prepend( html_title + html_body_start + html_body_no + html_body_end + html_body_endend );
+                }
+            }
+        }
+    );
 }
 
 /**
@@ -192,19 +272,19 @@ function contentEval( source ) {
     document.body.removeChild(script);
 }
 
-/**
- * 判断执行模块
- * @type {[type]}
- */
-var domain = document.domain;
-var ArrHost = domain.split(".");
-var emHost = ArrHost[ArrHost.length - 2].toString();
+var emHost = getHostName();
 switch (emHost) {
     case "qq":
         qzoneExcute();
         break;
     case "douban":
-        dbMovie();
-        contentEval( dbIsak );
+        var cname = getHostName(1);
+        if (cname == "movie") {
+            dbMovie();
+            dbIsakMovie();
+        } 
+        else {
+            contentEval( dbIsak );
+        }
         break;
 }
